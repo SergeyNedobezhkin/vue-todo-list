daa76766838a8c9cb1c640bdb54bab96d807776c
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-06-20 14:41:55 +0400

    Реализовал сохранение данных при перезагрузке стриницы

--- src/store/index.js
+++ src/store/index.js
@@ -8,23 +8,24 @@ export default new Vuex.Store({
     todos: [],
   },
   getters: {
-    allTodos: (state) => state.todos,
+    allTodos: (state) => {
+      const res = localStorage.getItem("todos");
+      state.todos = JSON.parse(res);
+      return state.todos;
+    },
   },
 
   actions: {
     addTodo({ commit }, todo) {
       commit("add_todo", todo);
       localStorage.setItem("todos", JSON.stringify(this.state.todos));
-      // const data = localStorage.getItem("todos");
-      // this.state.todos = JSON.parse(data);
-      // console.log(data);
     },
 
     deleteTodo({ commit }, id) {
       commit("delete_todo", id);
-
       localStorage.setItem("todos", JSON.stringify(this.state.todos));
     },
+
     updateTodo({ commit }, todo) {
       commit("update_todo", todo);
     },
@@ -43,12 +44,6 @@ export default new Vuex.Store({
       }
     },
   },
-
-  // mounted() {
-  //   const data = localStorage.getItem("todos");
-  //   this.state.todos = JSON.parse(data);
-  //   console.log(data);
-  // },
 
   modules: {},
 });
